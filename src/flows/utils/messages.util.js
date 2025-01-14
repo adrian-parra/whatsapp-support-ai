fb3e35b87d7551f7da03c743bdd45cbbfb7ff5bf
adrian-parra <adrian.adrianborquez@hotmail.com>
2025-01-13 18:27:43 -0700

    change(src/flows/utils/messages.util.js)
    * Se agrego type info y notification en funcion formatMessage() ,para esos casos especificos

--- src/flows/utils/messages.util.js
+++ src/flows/utils/messages.util.js
@@ -1,22 +1,21 @@
 /**
  * Genera un mensaje formateado.
- * @param {{ header: string; body: string; footer?: string; type: 'success' | 'error' }} options - Objeto con los datos del mensaje.
+ * @param {{ header: string; body: string; footer?: string; type: 'success' | 'error' | 'info' | 'notification'}} options - Objeto con los datos del mensaje.
  * @param {string} options.header - Encabezado del mensaje.
  * @param {string} options.body - Cuerpo del mensaje.
  * @param {string} [options.footer] - Pie de página del mensaje.
- * @param {'success' | 'error'} options.type - Tipo de mensaje ('success' o 'error').
+ * @param {'success' | 'error' | 'info' | 'notification'} options.type - Tipo de mensaje ('success', 'error' o 'info').
  * @returns {string} - Mensaje formateado.
  */
-export const formatMessage = ({ header, body, footer = '', type = 'success' }) => {
-  const prefix = type === 'success' ? '✅' : '❌';
+export const formatMessage = ({ header, body, footer = ' ', type = 'success' }) => {
+  const prefix = type === 'success' ? '✅' : (type === 'error' ? '❌' : (type === 'info' ? 'ℹ️' : '🔔'));
   const emphasis = type === 'success' ? '*' : '*';
-  const defaultFooter = type === 'success' ? '👍 😃' : '⚠️ 🙁';
+  const defaultFooter = type === 'success' ? '👍 😃' : (type === 'error' ? '⚠️ 🙁' : (type === 'info' ? '🤔' : ''));
   const formattedHeader = `${prefix} ${emphasis}${header}${emphasis}`;
   const formattedFooter = footer || defaultFooter;
 
   return `${formattedHeader}
-
+  
 ${body}
-
 ${formattedFooter}`;
 };
