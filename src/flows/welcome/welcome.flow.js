834a86b27c95343dc06254eb05203dc8d24399ce
adrian-parra <adrian.adrianborquez@gmail.com>
2025-08-11 12:33:12 -0700

    fix: update lib

--- src/flows/welcome/welcome.flow.js
+++ src/flows/welcome/welcome.flow.js
@@ -1,5 +1,4 @@
 import { addKeyword, EVENTS } from '@builderbot/bot'
-import { chatWithAI21 } from '../../ai/ai21studio.js'
 
 export const welcomeFlow = addKeyword(EVENTS.WELCOME)
     .addAction(async (ctx, { flowDynamic }) => {
@@ -7,12 +6,30 @@ export const welcomeFlow = addKeyword(EVENTS.WELCOME)
         
         try {
             await flowDynamic([
-                '🤖 *Procesando tu mensaje...*',
-                'ℹ️ *Nota importante:* Esta IA no guarda el contexto de conversaciones anteriores, por lo que cada mensaje será tratado de forma independiente. Por favor, sé específico en tus preguntas.'
+                '🤖 *Mejorando tu texto...*',
+                // 'ℹ️  *Nota importante:* Esta IA no guarda el contexto de conversaciones anteriores, por lo que cada entrada se procesará de forma independiente. Por favor, proporciona texto claro para obtener mejores mejoras.'
             ])
+
+            //const textAI21 = await chatWithAI21({message})
+
+            const formData = new FormData()
+            formData.append("text", message)
             
-            const textAI21 = await chatWithAI21({message})
-            await flowDynamic(`🤖 ${textAI21}`)
+
+            const response = await fetch(process.env.URL_API_BACK+"gemini/correct-text", {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'POST',
+                body: JSON.stringify({ text: message }),
+            })
+
+            if (!response.ok) {
+                throw new Error(`Error en la petición: ${response.status} ${response.statusText}`)
+            }
+
+            const data = await response.json()
+            await flowDynamic(`${data.correctedText}`)
         } catch (error) {
             await flowDynamic('*¡Lo sentimos!* 🔧\n\nDisculpe, en este momento estamos experimentando dificultades técnicas para procesar su solicitud.\n\nLe agradecemos su comprensión y le invitamos a intentarlo nuevamente en unos momentos. 🙏\n\n_El equipo de soporte está trabajando para resolver este inconveniente._')
         }
